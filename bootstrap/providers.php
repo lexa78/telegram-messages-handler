b6525676ad0adc8502c85c9d9182acbf77fc3119
deadpool <deadpool_7_8@yahoo.com>
2025-12-01 13:29:31 +0300

    Fix providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\ChannelJobServiceProvider::class,
-    App\Providers\ExchangeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
